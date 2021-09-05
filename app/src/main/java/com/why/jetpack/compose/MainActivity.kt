1897989f3f5400c8f6b1f3de152afdc029f76507
WahyuSeptiadi <wahyusptd@gmail.com>
2021-09-05 22:13:43 +0700

    text field component

--- app/src/main/java/com/why/jetpack/compose/MainActivity.kt
+++ app/src/main/java/com/why/jetpack/compose/MainActivity.kt
@@ -1,20 +1,28 @@
 package com.why.jetpack.compose
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.outlined.Check
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import com.why.jetpack.compose.ui.theme.JetpackComposeTheme
-import com.why.jetpack.compose.ui.theme.Typography
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,23 +32,64 @@ class MainActivity : ComponentActivity() {
             }
             // A surface container using the 'background' color from the theme
             Surface(color = MaterialTheme.colors.background) {
-                Greeting("Android")
+                InitView()
             }
         }
     }
 }
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
-fun Greeting(name: String) {
+fun InitView() {
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "Hello JetpackCompose!")
-        Text(
-            text = "My Name is $name",
-            style = Typography.h6
+        var text by remember {
+            mutableStateOf("")
+        }
+        val context = LocalContext.current
+        val keyboardController = LocalSoftwareKeyboardController.current
+
+        // TextField, BasicTextField, OutlineTextField
+        TextField(
+            value = text,
+            onValueChange = {
+                text = it
+            },
+            placeholder = {
+                Text(text = "Type here...")
+            },
+            label = {
+                Text(text = "First Name")
+            },
+            leadingIcon = {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(
+                        imageVector = Icons.Filled.Person,
+                        contentDescription = "Person Name"
+                    )
+                }
+            },
+            trailingIcon = {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(
+                        imageVector = Icons.Outlined.Check,
+                        contentDescription = "Check"
+                    )
+                }
+            },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.Done
+            ),
+            keyboardActions = KeyboardActions(
+                onDone = {
+                    Toast.makeText(context, "done :)", Toast.LENGTH_SHORT).show()
+                    keyboardController?.hide()
+                }
+            )
         )
     }
 }
@@ -49,8 +98,6 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     JetpackComposeTheme {
-        Column {
-            Greeting("Wahyu Septiadi")
-        }
+        InitView()
     }
 }
